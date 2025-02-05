32824d3a363f940592719ce605551663a4e56e03
DanyelZini <danyelzini1@gmail.com>
2025-02-04 21:48:44 -0300

    Commit 02

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            Integer numeroConta;
+            String agenciaConta;
+            String nomeCliente;
+            Double saldoConta;
+
+            System.out.println("Digite o número da conta:");
+            numeroConta = scanner.nextInt();
+
+            System.out.println("Digite a agência da conta:");
+            agenciaConta = scanner.next();
+
+            System.out.println("Digite o nome do cliente:");
+            nomeCliente = scanner.nextLine();
+            nomeCliente += scanner.nextLine();
+
+            System.out.println("Digite o saldo da conta:");
+            saldoConta = scanner.nextDouble();
+
+            System.out.println("Conta criada com sucesso!");
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso Banco, sua agência é " + agenciaConta + ", conta " + numeroConta + " e seu saldo " + saldoConta + " ja está disponível para saque.");
+        }
+
     }
 }
